38e10a7c3a4c387871119b0cff3c46c82dc32bb2
QuocLeIT <quoc.le.2107@gmail.com>
2017-06-25 18:46:30 +0700

    Update Danh sách sp thắng đấu giá

--- WebDoAn/Controllers/WinProductController.cs
+++ WebDoAn/Controllers/WinProductController.cs
@@ -11,11 +11,39 @@ namespace WebDoAn.Controllers
 {
     public class WinProductController : Controller
     {
-        //
+       //
         // GET: /WinProduct/
         public ActionResult Index()
         {
-            return View();
+            var winpro = CurrentContext.GetWinPro();
+            
+            return View(winpro.ItemsOrderDetail);            
         }
-	}
+
+        //GET://ListWinProduct
+        public ActionResult ListWinProduct()
+        {
+            using (var ctx = new QLBHEntities())
+            {
+                var list = ctx.OrderDetails                  
+                    .ToList();               
+
+                return View(list);
+            }          
+        }
+
+        // GET: /List ID Order/
+        public ActionResult ListIDOder()
+        {
+            using (var ctx = new QLBHEntities())
+            {
+                var list = ctx.Orders
+                    .Where(o => o.UserID == CurrentContext.GetCurUser().f_ID)
+                    .ToList();
+
+                ViewBag.Listidoder = list;
+
+                return View("ListWinProduct", ViewBag.Listidoder);
+            }
+        }
 }
