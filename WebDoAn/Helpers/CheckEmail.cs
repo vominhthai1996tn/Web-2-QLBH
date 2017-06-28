dae37c80e41e71b15192c3f85226dc543d58117e
QuocLeIT <quoc.le.2107@gmail.com>
2017-06-29 00:54:54 +0700

    Cập nhật class CheckEmail

---
+++ WebDoAn/Helpers/CheckEmail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebDoAn.Models;
+
+namespace WebDoAn.Helpers
+{
+    public class CheckEmail
+    {
+        //check email có hợp lệ không
+        public bool checkEmailHopLe(string email)
+        {
+            using (QLBHEntities ctx = new QLBHEntities())
+            {
+                var us = ctx.Users
+                    .Where(i => i.f_Email == email)
+                    .FirstOrDefault();
+
+                if (us != null)
+                    return false;
+            }
+
+            return true;
+        }
+        
+    
+    }
+}
